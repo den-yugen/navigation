e01f83e8952c7c7b61d647cf7eb2b0d9f5a5cf5b
Denis Yūgen <quirky-09triads@icloud.com>
2023-02-16 18:09:24 +0300

    StackView added

--- Navigation/Screens/FeedViewController.swift
+++ Navigation/Screens/FeedViewController.swift
@@ -5,30 +5,46 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-    private let postButton  = AppButton(backgroundColor: .systemCyan, title: "Новость")
-    private let post        = Post(title: "Новость")
+    private let postButton = AppButton(backgroundColor: .systemCyan, title: "Новость")
+    private let anotherPostButton = AppButton(backgroundColor: .systemMint, title: "Новость")
+    private let post = Post(title: "Новость")
+    private let buttonsStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 10
+        stack.distribution = .fillEqually
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        configurePostButton()
+        configureLayout()
+        configureConstraints()
     }
 
-    private func configurePostButton() {
-        view.addSubview(postButton)
+    private func configureLayout() {
+        view.addSubview(buttonsStack)
+        buttonsStack.addArrangedSubview(postButton)
+        buttonsStack.addArrangedSubview(anotherPostButton)
         postButton.addTarget(self, action: #selector(pushPostVC), for: .touchUpInside)
+        anotherPostButton.addTarget(self, action: #selector(pushPostVC), for: .touchUpInside)
+    }
 
+    private func configureConstraints() {
         NSLayoutConstraint.activate([
-            postButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            postButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            postButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            postButton.heightAnchor.constraint(equalToConstant: 50)
+            buttonsStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            buttonsStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            buttonsStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            buttonsStack.heightAnchor.constraint(equalToConstant: 110)
         ])
     }
 
     @objc private func pushPostVC() {
-        let postVC      = PostViewController()
-        postVC.title    = post.title
+        let postVC = PostViewController()
+        postVC.title = post.title
         navigationController?.pushViewController(postVC, animated: true)
     }
 }
